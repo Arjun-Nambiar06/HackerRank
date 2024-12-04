4918d89d49c506423731d548684687f4c391976e
Arjun_Nambiar <104570285+Arjun-Nambiar06@users.noreply.github.com>
2024-12-04 16:58:21 +0530

    PalindromeIndex Correction

--- Sheet 2/PalindromeIndex.py
+++ Sheet 2/PalindromeIndex.py
@@ -1,18 +1,24 @@
+def is_palindrome(s, l, r):
+    while l < right:
+        if s[l] != s[r]:
+            return False
+        l += 1
+        r -= 1
+    return True
+
 def palindromeIndex(s):
-    for i in range(len(s)):
-        l, r, skip = 0, len(s)-1, False
-        while l<r:
-            if l==i:
-                l+=1
-            if r==i:
-                r-=1
-            if s[l] != s[r]:
-                skip = True
-                break
-            l+=1
-            r-=1
-        if skip == False:
-            return i
+    l, r = 0, len(s) - 1
+    
+    while l < r:
+        if s[l] != s[r]:
+            if is_palindrome(s, l + 1, r):
+                return l
+            elif is_palindrome(s, l, r - 1):
+                return r
+            else:
+                return -1
+        l += 1
+        r -= 1
     return -1
 
 str = input("Enter a string: ")
